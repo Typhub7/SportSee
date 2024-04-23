73948f0072945f64520a983aaf6d130dec5a0ef2
Typhub7 <drevon.caroline@gmail.com>
2024-04-23 09:05:18 +0200

    improve RadarChart for smallconfig

--- src/components/RadarChart/RadarChart.tsx
+++ src/components/RadarChart/RadarChart.tsx
@@ -3,6 +3,7 @@ import { UserPerformanceData } from "../../model/UserPerformanceModel";
 import { mapUserPerformance } from '../../utils/performanceUtils';
 import './radarchart.css'
 import { ResponsiveContainer } from 'recharts';
+import { useEffect, useState } from 'react';
 
 /** 
  * Radar chart component displaying user performance data.
@@ -12,13 +13,34 @@ import { ResponsiveContainer } from 'recharts';
  */
 const ChartRadar = ({ UserPerformance }: { UserPerformance: UserPerformanceData }) => {
     const mappedUserData = mapUserPerformance(UserPerformance)
-    
-	return (
+    // Vérifie la largeur de la fenêtre pour décider de la taille du tickSize
+    const tickSize = window.innerWidth <= 1300 ? 16 : 10; 
+    // Ajuste le outerRadius en fonction de la largeur de l'écran
+    const outerRadius = window.innerWidth <= 1300 ? '53%' : '73%'; 
+
+    // Met à jour polarRadius lors du montage et démontage du composant en réponse aux changements de taille de la fenêtre
+    function calculatePolarRadius() {
+        return window.innerWidth <= 1300 ? [4, 10, 28, 43, 58] : [9, 20, 45, 65, 90];
+    }
+	const [polarRadius, setPolarRadius] = useState(calculatePolarRadius());
+    useEffect(() => {
+        const handleResize = () => {
+            setPolarRadius(calculatePolarRadius());
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); 
+
+    return (
 		<div className='radarChartContainer'>
             <ResponsiveContainer width="100%" height="100%">
-                <RadarChart  cx="50%" cy="50%" innerRadius="10%" outerRadius="73%" data={mappedUserData.performance}>
-                    <PolarGrid radialLines={false} polarRadius={[9,20,45,65,90]} />
-                    <PolarAngleAxis dataKey="kind" dy={2} tickSize={1} tick={{ fill: 'white' }} />
+                <RadarChart  cx="50%" cy="50%" innerRadius="10%" outerRadius={outerRadius} data={mappedUserData.performance}>
+                    <PolarGrid radialLines={false} polarRadius={polarRadius} />
+                    <PolarAngleAxis dataKey="kind" dy={2} tickSize={tickSize} tick={{ fill: 'white' }} />
                     <Radar name="kind" dataKey="value" fill="red" fillOpacity={0.7} />
                 </RadarChart>
             </ResponsiveContainer>
